cfd341db4b491b5f5b7aff1717a045f35249e7b3
Max <max@privatevoid.net>
2025-02-12 23:55:02 +0100

    nix: remove some bullshit

--- nix/shell.nix
+++ nix/shell.nix
@@ -4,10 +4,9 @@
 let
   inherit (pkgs) lib;
 in
-pkgs.mkShell rec {
+pkgs.mkShell.override { stdenv = pkgs.useMoldLinker pkgs.stdenv; } {
   packages =
     [
-      pkgs.clang
       pkgs.curl
       pkgs.cmake
       pkgs.perl
@@ -15,9 +14,6 @@ pkgs.mkShell rec {
       pkgs.protobuf
       pkgs.rustPlatform.bindgenHook
       pkgs.rust-analyzer
-    ]
-    ++ lib.optionals pkgs.stdenv.hostPlatform.isLinux [
-      pkgs.mold
     ];
 
   buildInputs =
@@ -29,7 +25,6 @@ pkgs.mkShell rec {
       pkgs.libgit2
       pkgs.openssl
       pkgs.sqlite
-      pkgs.stdenv.cc.cc
       pkgs.zlib
       pkgs.zstd
       pkgs.rustToolchain
@@ -43,23 +38,7 @@ pkgs.mkShell rec {
     ]
     ++ lib.optional pkgs.stdenv.hostPlatform.isDarwin pkgs.apple-sdk_15;
 
-  LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
-
   PROTOC="${pkgs.protobuf}/bin/protoc";
 
-  # We set SDKROOT and DEVELOPER_DIR to the Xcode ones instead of the nixpkgs ones,
-  # because we need Swift 6.0 and nixpkgs doesn't have it.
-  # Xcode is required for development anyways
-  shellHook = lib.optionalString pkgs.stdenv.hostPlatform.isDarwin ''
-      export SDKROOT="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk";
-      export DEVELOPER_DIR="/Applications/Xcode.app/Contents/Developer";
-    '';
-
-  FONTCONFIG_FILE = pkgs.makeFontsConf {
-    fontDirectories = [
-      "./assets/fonts/zed-mono"
-      "./assets/fonts/zed-sans"
-    ];
-  };
   ZSTD_SYS_USE_PKG_CONFIG = true;
 }

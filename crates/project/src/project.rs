abb19a19ad9f2241c129f85619033f7762dc3b71
Max <max@privatevoid.net>
2025-06-22 00:03:57 +0200

    crates/project: remove telemetry

--- crates/project/src/project.rs
+++ crates/project/src/project.rs
@@ -2943,9 +2943,6 @@ impl Project {
             WorktreeStoreEvent::WorktreeOrderChanged => cx.emit(Event::WorktreeOrderChanged),
             WorktreeStoreEvent::WorktreeUpdateSent(_) => {}
             WorktreeStoreEvent::WorktreeUpdatedEntries(worktree_id, changes) => {
-                self.client()
-                    .telemetry()
-                    .report_discovered_project_events(*worktree_id, changes);
                 cx.emit(Event::WorktreeUpdatedEntries(*worktree_id, changes.clone()))
             }
             WorktreeStoreEvent::WorktreeDeletedEntry(worktree_id, id) => {

c186dd160afbd1efc4a0e6b201cd557ba571aa28
Max <max@privatevoid.net>
2025-06-22 03:00:20 +0200

    crates/feedback: remove SystemSpecs

--- crates/feedback/src/feedback.rs
+++ crates/feedback/src/feedback.rs
@@ -1,17 +1,12 @@
-use gpui::{App, ClipboardItem, PromptLevel, actions};
-use system_specs::SystemSpecs;
-use util::ResultExt;
+use gpui::{App, actions};
 use workspace::Workspace;
 use zed_actions::feedback::FileBugReport;
 
 pub mod feedback_modal;
 
-pub mod system_specs;
-
 actions!(
     zed,
     [
-        CopySystemSpecsIntoClipboard,
         OpenZedRepo,
         RequestFeature,
     ]
@@ -30,27 +25,6 @@ pub fn init(cx: &mut App) {
         };
         feedback_modal::FeedbackModal::register(workspace, window, cx);
         workspace
-            .register_action(|_, _: &CopySystemSpecsIntoClipboard, window, cx| {
-                let specs = SystemSpecs::new(window, cx);
-
-                cx.spawn_in(window, async move |_, cx| {
-                    let specs = specs.await.to_string();
-
-                    cx.update(|_, cx| {
-                        cx.write_to_clipboard(ClipboardItem::new_string(specs.clone()))
-                    })
-                    .log_err();
-
-                    cx.prompt(
-                        PromptLevel::Info,
-                        "Copied into clipboard",
-                        Some(&specs),
-                        &["OK"],
-                    )
-                    .await
-                })
-                .detach();
-            })
             .register_action(|_, _: &RequestFeature, _, cx| {
                 cx.open_url(REQUEST_FEATURE_URL);
             })

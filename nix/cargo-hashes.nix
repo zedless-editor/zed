64b3d3c7ec904430e8e40fa6040df26306023421
Max <max@privatevoid.net>
2025-08-13 02:14:05 +0200

    nix: update cargo hashes

--- nix/cargo-hashes.nix
+++ nix/cargo-hashes.nix
@@ -3,18 +3,18 @@
   "async-pipe-0.1.3" = "sha256-g120X88HGT8P6GNCrzpS5SutALx5H+45Sf4iSSxzctE=";
   "async-stripe-0.40.0" = "sha256-kVdYCmlM8DilSrcOGxI1tvSiUjSrXdnmKqBA9WUJgMw=";
   "blade-graphics-0.6.0" = "sha256-LwJEok+iIuJqmz2BSc7vRZVtZIvmyaXlxDcoWuX+itk=";
-  "dap-types-0.0.1" = "sha256-w/4B5wvB2+keLvbk97FfN1GHodSwuBUm0MuXQoCI1xY=";
+  "dap-types-0.0.1" = "sha256-H8QwXMcq5CH8qjzNw1eXQhNhPet+662kmn5LomIBzGo=";
   "font-kit-0.14.1" = "sha256-/l1o3L4ZRMfmoOJqCDup3dCU3rXG4FUznEvRkHWzHBQ=";
   "jj-lib-0.29.0" = "sha256-+PChmZ46pH4z2MSvT9KNaz2HH29kRf4vZ+2fXxR/SAY=";
   "jupyter-protocol-0.6.0" = "sha256-v+avgSYYT9BJ1+lIEF+Vp1V9D0YH8LGH4XEjweN/pLs=";
-  "libwebrtc-0.3.10" = "sha256-xXRfR750NFxE+lbcB8guP/W7OZsg2bRJzCRS106OZF4=";
+  "libwebrtc-0.3.10" = "sha256-DNC1SfTHpAzO5qFeUBivmr+O+Dar9gAJpjGVd8REBKw=";
   "lsp-types-0.95.1" = "sha256-pLavGU2/96Qzi107rN0erbUzXcApVpfSUZrLgu4hUdU=";
   "notify-8.0.0" = "sha256-09ai0jQ9ti63SvP30fcNDOKagypg6N1T6TRzUW9GVeA=";
   "nvim-rs-0.9.2" = "sha256-cnG18TL/L4NUze4ltz4IKo2uLhuRwmwZd/yNWrCweC4=";
   "pet-0.1.0" = "sha256-DVLyPNQLzYuebe9wVldYyhKD09IoUZL19J1jwDgHOeU=";
   "reqwest-0.12.15" = "sha256-G7RRaNP6aqixSThXFJybPZ1AnesYn+f97p210FHf3qw=";
   "scap-0.0.8" = "sha256-3DdcZoDImYuP+QmdsyaoMDiL61xqkC3zodDi9CgWiXg=";
-  "tiktoken-rs-0.8.0" = "";
+  "tiktoken-rs-0.8.0" = "sha256-3nbhoyvIJkRL1vdQhbAENnvaQCnIDKPqlyuQgdzu/j4=";
   "tree-sitter-gitcommit-0.0.1" = "sha256-Z1BDWFXve4lRa6cTQyVHAfVo2ZFhcxKdRYpyWqbU4lY=";
   "tree-sitter-gomod-1.1.1" = "sha256-KD6Fio9qie3wbGAvQaYsMdYOK1QjnRrGExt1oL/6mis=";
   "tree-sitter-gowork-0.0.1" = "sha256-WRMgGjOlJ+bT/YnSBeSLRTLlltA5WwTvV0Ow/949+BE=";
@@ -24,5 +24,5 @@
   "tree-sitter-yaml-0.6.1" = "sha256-95u/bq74SiUHW8lVp3RpanmYS/lyVPW0Inn8gR7N3IQ=";
   "windows-capture-1.4.3" = "sha256-k07Jpk4mcDo1JsZDF7AZMHRqlqFKHt60G2X3BUjMhBc=";
   "xim-0.4.0" = "sha256-BXyaIBoqMNbzaSJqMadmofdjtlEVSoU6iogF66YP6a4=";
-  "yawc-0.2.4" = "";
+  "yawc-0.2.4" = "sha256-C/iUVbny697xkZMD5fdv69/cQZ0YlxX7X4BzF9gHXc4=";
 }
